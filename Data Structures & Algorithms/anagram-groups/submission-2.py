62b2f95ca9a95ac2f4d5c3bcc2c5b5ceec622733
Phuc Pham <phuc.phamhuythienai@gmail.com>
2026-08-06 13:01:23 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dct=defaultdict(list)
+        for word in strs:
+            key = ''.join(sorted(word))
+            dct[key].append(word)
+        return list(dct.values())
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dct = {}
+        for i, num in enumerate(nums):
+            complementary = target - num
+            if complementary in dct:
+                return [dct[complementary], i]
+            dct[num] = i
+        

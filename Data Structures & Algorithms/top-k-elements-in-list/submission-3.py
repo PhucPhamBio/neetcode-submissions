984f88bde1587d63dbfa41a7f2f0b55f8abebe5e
Phuc Pham <phuc.phamhuythienai@gmail.com>
2026-08-12 11:41:10 -0500

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,7 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqCount = Counter(nums)
+        return heapq.nlargest(k, freqCount.keys(), key = freqCount.get)

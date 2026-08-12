90096a7919e64df4dbfb77ba7ed444d950fbe7b0
Phuc Pham <phuc.phamhuythienai@gmail.com>
2026-08-12 11:43:37 -0500

    Add: string-encode-and-decode - submission-17

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-17.py
@@ -0,0 +1,12 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        return ''.join(['{:4}{}'.format(len(s), s) for s in strs])
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        ans = [] 
+        while i < len(s):
+            length = int(s[i:i+4])
+            i = i + 4
+            ans.append(s[i:i+length])
+            i = i + length
+        return ans
